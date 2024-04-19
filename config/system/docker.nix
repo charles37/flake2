de5f8027e5971cb0c5f410f5ffba918a28600e61
charles37 <benprevor@gmail.com>
2024-04-19 17:49:56 -0400

    add docker

--- config/system/docker.nix
+++ config/system/docker.nix
@@ -1,17 +1,18 @@
-{ pkgs, config, username, ... }:
-
+{
+  pkgs,
+  config,
+  username,
+  ...
+}:
 #let inherit (import ../../options.nix) theKBDVariant
 #theKBDLayout theSecondKBDLayout; in
-let inherit (import ../../options.nix); 
-
-in
-{
+let
+  inherit (import ../../options.nix);
+in {
   virtualisation.docker.enable = true;
-  users.extraGroups.docker.members = [ "${username}" ];
+  users.extraGroups.docker.members = ["${username}"];
   virtualisation.docker.rootless = {
     enable = true;
     setSocketVariable = true;
   };
 }
-
-
