06897411fd8d9d0254837d83efe228be3aa0cdd6
charles37 <benprevor@gmail.com>
2024-06-06 13:25:07 -0400

    added udev rules for tock os

--- config/system/services.nix
+++ config/system/services.nix
@@ -1,15 +1,20 @@
-{ pkgs, config, lib, ... }:
-
 {
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
   # List services that you want to enable:
   services.openssh.enable = true;
   services.fstrim.enable = true;
   xdg.portal = {
     enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal
     ];
-    configPackages = [ pkgs.xdg-desktop-portal-gtk
+    configPackages = [
+      pkgs.xdg-desktop-portal-gtk
       pkgs.xdg-desktop-portal-hyprland
       pkgs.xdg-desktop-portal
     ];
@@ -27,7 +32,7 @@
   programs.thunar.enable = true;
   services.gvfs.enable = true;
   services.tumbler.enable = true;
-  services.gnome.gnome-keyring.enable=true;
+  services.gnome.gnome-keyring.enable = true;
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   services.blueman.enable = true;
@@ -35,5 +40,14 @@
     text = ''
       auth include login
     '';
+  };
+  services.udev = {
+    extraRules = builtins.concatStringsSep "\n" [
+      # Tock device rules for CMSIS-DAP devices
+      ''ACTION!="add|change",GOTO="openocd_rules_end"''
+      ''SUBSYSTEM!="usb|tty|hidraw",GOTO="openocd_rules_end"''
+      ''ATTRS[product]=="CMSIS-DAP*",MODE="664", GROUP="plugdev"''
+      ''LABEL="openocd_rules_end"''
+    ];
   };
 }
